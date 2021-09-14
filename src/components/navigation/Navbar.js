c191d42ba400f85e225da1fc86bea7a4185067d9
bogdaniordan <bogdan.iordan47@yahoo.com>
2021-09-14 13:04:22 +0300

    Navbar options change if the user is logged in

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,11 +1,10 @@
 import React, {useEffect, useState} from 'react';
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
 import logo from '../../images/logo.svg';
+import AuthService from "../../service/AuthService";
 
-const Navbar = ({ color, homePosition, userHasApplied, applyBtn }) => {
-    // const loggedIn = !!(localStorage.getItem("joblishUser"))
-    // const loggedInComp = !!(localStorage.getItem("joblisComp"))
-
+const Navbar = ({ homePosition }) => {
+    const history = useHistory();
     const [position, setPosition] = useState("static")
     const [stickyColor, setStickyColor] = useState("rgba(0, 0, 255, 0.534)")
     const [stickyBorder, setStickyBorder] = useState("transparent")
@@ -29,7 +28,7 @@ const Navbar = ({ color, homePosition, userHasApplied, applyBtn }) => {
             else {
                 setPosition("static")
             }
-            setStickyColor(color)
+            // setStickyColor(color)
             setStickyBorder("transparent")
         }
     }
@@ -41,41 +40,29 @@ const Navbar = ({ color, homePosition, userHasApplied, applyBtn }) => {
         window.addEventListener('scroll', handleScroll)
     }, [])
 
+    const logout = () => {
+        AuthService.logout();
+        history.push("/login")
+    }
+
     return (
         <nav style={{backgroundColor:"" + stickyColor, position:"" + position, zIndex:"3", borderBottom: stickyBorder , animation:position === "sticky" ? "fadein 0.3s" : "none"}}>
             <div className="logo">
                 <Link to="/" style={{display:"inline-block"}}><img src={logo} style={{zIndex:"2", width:"50px",display:"inline-block"}}  alt="joblish logo"/>joblish</Link>
             </div>
-            <ul>
-                <li><Link to="/profile">Company</Link></li>
-            </ul>
-            {/*{*/}
-            {/*    // loggedInComp ? (<ul>*/}
-            {/*    //     <li><Link to="/jobs">Jobs</Link></li>*/}
-            {/*    //     <li><Link to="/" onClick={() => { localStorage.clear() }}>Logout</Link></li>*/}
-            {/*    // </ul>) : (*/}
-            {/*    //*/}
-            {/*    //     loggedIn ? (*/}
-            {/*            <ul>*/}
-            {/*                <li><Link to="/jobs">Jobs</Link></li>*/}
-            {/*                <li><Link to="/account">My profile</Link></li>*/}
-            {/*                <li><Link to="/user-applications">Applications</Link></li>*/}
-            {/*                <li><Link to="/companies">Companies</Link></li>*/}
-            {/*                <li><Link to="/" onClick={() => { localStorage.clear() }}>Logout</Link></li>*/}
-            {/*                /!*{!userHasApplied  && position == "sticky" && window.location.pathname.match("/job/") ? (*!/*/}
-            {/*                /!*    <li> <div onClick={applyBtn} className="apply-nav"><SendIcon /><h5 className="btn-title">Apply</h5></div></li>*!/*/}
-            {/*                /!*) : (*!/*/}
-            {/*                /!*    ""*!/*/}
-            {/*                /!*)}*!/*/}
-            {/*            </ul>*/}
-            {/*        /!*) : (*!/*/}
-            {/*        /!*    <ul>*!/*/}
-            {/*        /!*        <li><Link to="/jobs">Jobs</Link></li>*!/*/}
-            {/*        /!*        <li><Link to="/login">Login</Link></li>*!/*/}
-            {/*        /!*        <li><Link to="/register">Register</Link></li>*!/*/}
-            {/*        /!*    </ul>*!/*/}
-            {/*        /!*))*!/*/}
-            {/*}*/}
+            {
+                AuthService.getCurrentUser() ? (
+                    <ul>
+                        <li><Link to="/profile">Company</Link></li>
+                        <li><Link onClick={logout}>Logout</Link></li>
+                    </ul>
+                ) : (
+                    <ul>
+                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to="/register">Register</Link></li>
+                    </ul>
+                )
+            }
         </nav >
     )
 };
