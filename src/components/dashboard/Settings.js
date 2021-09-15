4c75221ff4c99b264908f8cfce0fd048cd2fbfce
bogdaniordan <bogdan.iordan47@yahoo.com>
2021-09-15 10:56:36 +0300

    Fixed change password tab

--- src/components/dashboard/Settings.js
+++ src/components/dashboard/Settings.js
@@ -11,102 +11,77 @@ import {
   TextField
 } from '@material-ui/core';
 import {useForm} from "react-hook-form";
+import CompanyService from "../../service/CompanyService";
+import AuthService from "../../service/AuthService";
+import {useHistory} from "react-router-dom";
 
-const Settings = (props) => {
-
-    const [firstPassword, setFirstPassword] = useState("");
-    const [secondPassword, setSecondPassword] = useState("");
-
-    // const [values, setValues] = useState({
-  //   password: '',
-  //   confirm: ''
-  // });
-  //
-  // const handleChange = (event) => {
-  //   setValues({
-  //     ...values,
-  //     [event.target.name]: event.target.value
-  //   });
-  // };
+const Settings = () => {
+    const history = useHistory();
     const { register, handleSubmit, formState: {errors} } = useForm();
 
     return (
         <>
         <Navbar />
-            {/*<div style={{display:"flex", flexDirection:"row"}}>*/}
-            {/*<SideBar/>*/}
-            {/*<div className="main-body">*/}
-            {/*    <div className="page-wrapper">*/}
-            {/*        <div className="flex-body"></div>*/}
-            {/*            <form {...props}>*/}
-            {/*                <Card>*/}
-            {/*                    <CardHeader*/}
-            {/*                    subheader="Update password"*/}
-            {/*                    title="Password"*/}
-            {/*                    />*/}
-            {/*                    <Divider />*/}
-            {/*                    <CardContent>*/}
-            {/*                    <TextField*/}
-            {/*                        fullWidth*/}
-            {/*                        label="Password"*/}
-            {/*                        margin="normal"*/}
-            {/*                        name="firstPassword"*/}
-            {/*                        onChange={e => setFirstPassword(e.target.value)}*/}
-            {/*                        type="password"*/}
-            {/*                                // value={values.password}*/}
-            {/*                        variant="outlined"*/}
-            {/*                        {...register("firstPassword", {*/}
-            {/*                            required: true,*/}
-            {/*                            minLength: 5,*/}
-            {/*                            maxLength: 35,*/}
-            {/*                            validate: {*/}
-            {/*                                positive: firstPassword === secondPassword*/}
-            {/*                            }*/}
-            {/*                        })}*/}
-            {/*                    />*/}
-            {/*                        {errors.firstPassword && <span style={{color:"red"}}>Please enter a password between 5 and 35 characters long, password have to also match each other!</span>}*/}
-            {/*                    <TextField*/}
-            {/*                        fullWidth*/}
-            {/*                        label="Confirm password"*/}
-            {/*                        margin="normal"*/}
-            {/*                        name="secondPassword"*/}
-            {/*                        onChange={e => setSecondPassword(e.target.value)}*/}
-            {/*                        type="password"*/}
-            {/*                        // value={values.confirm}*/}
-            {/*                        variant="outlined"*/}
-            {/*                    />*/}
-            {/*                        {...register("secondPassword", {*/}
-            {/*                            required: true,*/}
-            {/*                            minLength: 5,*/}
-            {/*                            maxLength: 35,*/}
-            {/*                            validate: {*/}
-            {/*                                positive: firstPassword === secondPassword*/}
-            {/*                            }*/}
-            {/*                        })}*/}
-            {/*                        />*/}
-            {/*                        {errors.secondPassword && <span style={{color:"red"}}>Please enter a password between 5 and 35 characters long, password have to also match each other!</span>}*/}
-            {/*                    </CardContent>*/}
-            {/*                    <Divider />*/}
-            {/*                    <Box*/}
-            {/*                    sx={{*/}
-            {/*                        display: 'flex',*/}
-            {/*                        justifyContent: 'flex-end',*/}
-            {/*                        p: 2*/}
-            {/*                    }}*/}
-            {/*                    >*/}
-            {/*                    <Button*/}
-            {/*                        color="primary"*/}
-            {/*                        variant="contained"*/}
-            {/*                        type="submit"*/}
-            {/*                    >*/}
-            {/*                        Update*/}
-            {/*                    </Button>*/}
-            {/*                    </Box>*/}
-            {/*                </Card>*/}
-            {/*            </form>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
+            <div style={{display:"flex", flexDirection:"row"}}>
+            <SideBar/>
+            <div className="main-body" style={{height: "700px"}}>
+                <div className="page-wrapper">
+                    <div className="flex-body"></div>
+                        <form onSubmit={
+                            handleSubmit((data) => {
+                                CompanyService.changePassword(AuthService.getCurrentUser().id, data)
+                                    .then(res => history.push("/dashboard"))
+                            })
+                        }>
+                            <Card>
+                                <CardHeader
+                                subheader="Update password"
+                                title="Password"
+                                />
+                                    <Divider />
+                                        <CardContent>
+                                            <TextField
+                                                fullWidth
+                                                label="Password"
+                                                margin="normal"
+                                                name="firstPassword"
+                                                type="password"
+                                                variant="outlined"
+                                                {...register("firstPassword", {required: true, minLength: 5, maxLength: 35})}
+                                            />
+                                                {errors.firstPassword && <span style={{color:"red"}}>Please enter a password between 5 and 35 characters long, password have to also match each other!</span>}
+                                            <TextField
+                                                fullWidth
+                                                label="Confirm password"
+                                                margin="normal"
+                                                name="secondPassword"
+                                                type="password"
+                                                variant="outlined"
+                                                {...register("secondPassword", {required: true, minLength: 5, maxLength: 35})}
+                                            />
+                                                {errors.secondPassword && <span style={{color:"red"}}>Please enter a password between 5 and 35 characters long, password have to also match each other!</span>}
+                                        </CardContent>
+                                    <Divider />
+                                <Box
+                                sx={{
+                                    display: 'flex',
+                                    justifyContent: 'flex-end',
+                                    p: 2
+                                }}
+                                >
+                                <Button
+                                    color="primary"
+                                    variant="contained"
+                                    type="submit"
+                                >
+                                    Update
+                                </Button>
+                                </Box>
+                            </Card>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </>
 
   );
